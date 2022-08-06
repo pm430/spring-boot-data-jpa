8496560f95b76108430e4077be0c7e538d2475e7
pm430 <open.js@gmail.com>
2022-08-07 00:17:09 +0900

    apply flywaydb.

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,6 @@
-drop table if exists account CASCADE
-drop sequence if exists hibernate_sequence
-create sequence hibernate_sequence start with 1 increment by 1
+drop table if exists account;
+drop sequence if exists hibernate_sequence;
+create sequence hibernate_sequence start with 1 increment by 1;
 create table account
 (
     id       bigint not null,
@@ -8,4 +8,4 @@ create table account
     password varchar(255),
     username varchar(255),
     primary key (id)
-)
+);
